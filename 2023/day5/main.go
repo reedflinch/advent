d2ee8f00d2eedfed1082f33ba68d849e318d474c
reedflinch <stflinch1@gmail.com>
2023-12-06 17:19:05 -0500

    day 5 part 2 works

--- 2023/day5/main.go
+++ 2023/day5/main.go
@@ -1,193 +1,102 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"log"
 	"math"
 	"os"
-	"reflect"
 	"strconv"
 	"strings"
-	"unicode"
 )
 
 // https://adventofcode.com/2023/day/5
 
-type Seeder struct {
-	Seed, Soil, Fertilizer, Water, Light, Temperature, Humidity, Location int64
-}
+var fileName = "input.txt"
 
 type Entry struct {
 	sourceStart, destStart, length int64
 }
 
-type Pair struct {
-	src, dest string
-}
+type Mapping []Entry
 
-var pairs = []Pair{
-	{src: "Seed", dest: "Soil"},
-	{src: "Soil", dest: "Fertilizer"},
-	{src: "Fertilizer", dest: "Water"},
-	{src: "Water", dest: "Light"},
-	{src: "Light", dest: "Temperature"},
-	{src: "Temperature", dest: "Humidity"},
-	{src: "Humidity", dest: "Location"},
-}
-
-var fileName = "input.txt"
-var seeds []Seeder
-
-func main() {
-	runPart(1)
-	runPart(2)
+type Almanac struct {
+	seedToSoil            Mapping
+	soilToFertilizer      Mapping
+	fertilizerToWater     Mapping
+	waterToLight          Mapping
+	lightToTemperature    Mapping
+	temperatureToHumidity Mapping
+	humidityToLocation    Mapping
 }
 
 func runPart(part int) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
-	s := bufio.NewScanner(file)
-
-	seeds = []Seeder{}
-	seeds = append(seeds, getSeeds(s, part)...)
-
-	for _, pair := range pairs {
-		// fmt.Printf("processing pair %+v part %v\n", pair, part)
-		processPair(s, pair)
-	}
-
-	fmt.Printf("lowest Location (part %v) = %v\n", part, getLowestLocation())
-}
-
-// 	for _, seed := range seeds {
-
-// 	}
-
-// func (s *Seeder) traverse() {
-
-// }
-
-func processPair(sc *bufio.Scanner, p Pair) {
-	src, dest := p.src, p.dest
-
-	headerName := fmt.Sprintf("%s-to-%s map:", strings.ToLower(src), strings.ToLower(dest))
-
-	mapping := []Entry{}
-	foundMap := false
-
-	// extract data from input
-	for sc.Scan() {
-		if strings.HasPrefix(sc.Text(), headerName) {
-			foundMap = true
-			continue
-		}
-
-		isEntryLine := len(sc.Text()) > 0 && unicode.IsDigit(rune(sc.Text()[0]))
-		if foundMap {
-			if !isEntryLine {
-				break
-			} else {
-				// populate current map
-				mapping = append(mapping, parseEntry(sc.Text()))
-			}
-		}
-	}
-
-	// process all seeds
-	for i, seed := range seeds {
-		found := false
-		srcValue := reflect.ValueOf(seed).FieldByName(src).Int()
-
-		for _, entry := range mapping {
-			if srcValue >= entry.sourceStart && srcValue <= entry.sourceStart+entry.length-1 {
-				newValue := srcValue + (entry.destStart - entry.sourceStart)
-				seeds[i].setProperty(dest, newValue)
-				found = true
-				break
-			}
-		}
-		if !found {
-			seeds[i].setProperty(dest, srcValue)
-		}
-	}
-}
-
-func getSeeds(sc *bufio.Scanner, part int) []Seeder {
-	if part == 1 {
-		fmt.Printf("getting seeds part 1...\n")
-		return getSeedsPart1(sc)
-	}
-	fmt.Printf("getting seeds part 2...\n")
-	return getSeedsPart2(sc)
-}
-
-func getSeedsPart1(sc *bufio.Scanner) []Seeder {
-	line := ""
-	allSeeds := []Seeder{}
-
-	for sc.Scan() {
-		if strings.HasPrefix(sc.Text(), "seeds:") {
-			line = sc.Text()
-			break
-		}
-	}
-
-	seedList := strings.TrimSpace(strings.TrimPrefix(line, "seeds:"))
-	seedStrs := strings.Fields(seedList)
-
-	for _, seedStr := range seedStrs {
-		id := parseInt(seedStr)
-		allSeeds = append(allSeeds, Seeder{Seed: id})
-	}
-
-	return allSeeds
-}
-
-func getSeedsPart2(sc *bufio.Scanner) []Seeder {
-	line := ""
-	allSeeds := []Seeder{}
-
-	for sc.Scan() {
-		if strings.HasPrefix(sc.Text(), "seeds:") {
-			line = sc.Text()
-			break
-		}
-	}
-
-	seedList := strings.TrimSpace(strings.TrimPrefix(line, "seeds:"))
-	seedStrs := strings.Fields(seedList)
-
-	for i := 0; i < len(seedStrs)-1; i += 2 {
-		start := parseInt(seedStrs[i])
-		length := parseInt(seedStrs[i+1])
-
-		for j := start; j < start+length; j++ {
-			fmt.Printf("adding seed %v\n", j)
-			allSeeds = append(allSeeds, Seeder{Seed: j})
-		}
-	}
-
-	return allSeeds
-}
-
-func getLowestLocation() int64 {
 	lowest := math.MaxInt64
 
+	bytes, err := os.ReadFile(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := string(bytes)
+	content = strings.Trim(content, "\n")
+	groups := strings.Split(content, "\n\n")
+
+	seeds := []int64{}
+	seeds = append(seeds, getSeeds(groups[0], part)...)
+
+	a := Almanac{
+		groupToMapping(groups[1]),
+		groupToMapping(groups[2]),
+		groupToMapping(groups[3]),
+		groupToMapping(groups[4]),
+		groupToMapping(groups[5]),
+		groupToMapping(groups[6]),
+		groupToMapping(groups[7]),
+	}
+
 	for _, seed := range seeds {
-		if seed.Location < int64(lowest) {
-			lowest = int(seed.Location)
+		location := a.traverse(seed)
+		if location < int64(lowest) {
+			lowest = int(location)
 		}
 	}
 
-	return int64(lowest)
+	fmt.Printf("lowest location (part %v) = %v\n", part, lowest)
 }
 
-func (s *Seeder) setProperty(name string, value int64) *Seeder {
-	reflect.ValueOf(s).Elem().FieldByName(name).SetInt(value)
-	return s
+func (m Mapping) get(val int64) int64 {
+	for _, entry := range m {
+		if val >= entry.sourceStart && val <= entry.sourceStart+entry.length-1 {
+			newVal := val + (entry.destStart - entry.sourceStart)
+			return newVal
+		}
+	}
+
+	return val
+}
+
+func (a *Almanac) traverse(seed int64) (location int64) {
+	soil := a.seedToSoil.get(seed)
+	fertilizer := a.soilToFertilizer.get(soil)
+	water := a.fertilizerToWater.get(fertilizer)
+	light := a.waterToLight.get(water)
+	temperature := a.lightToTemperature.get(light)
+	humidity := a.temperatureToHumidity.get(temperature)
+	location = a.humidityToLocation.get(humidity)
+
+	return location
+}
+
+func groupToMapping(group string) Mapping {
+	lines := strings.Split(group, "\n")
+	mappingLines := lines[1:]
+
+	mappings := make(Mapping, 0, len(mappingLines))
+	for _, line := range mappingLines {
+		entry := parseEntry(line)
+		mappings = append(mappings, entry)
+	}
+
+	return mappings
 }
 
 func parseEntry(s string) Entry {
@@ -199,13 +108,53 @@ func parseEntry(s string) Entry {
 	}
 }
 
+func getSeeds(line string, part int) []int64 {
+	if part == 1 {
+		fmt.Printf("getting seeds part 1...\n")
+		return getSeedsPart1(line)
+	}
+	fmt.Printf("getting seeds part 2...\n")
+	return getSeedsPart2(line)
+}
+
+func getSeedsPart1(line string) []int64 {
+	allSeeds := []int64{}
+
+	seedList := strings.TrimSpace(strings.TrimPrefix(line, "seeds:"))
+	seedStrs := strings.Fields(seedList)
+
+	for _, seedStr := range seedStrs {
+		id := parseInt(seedStr)
+		allSeeds = append(allSeeds, id)
+	}
+
+	return allSeeds
+}
+
+func getSeedsPart2(line string) []int64 {
+	allSeeds := []int64{}
+
+	seedList := strings.TrimSpace(strings.TrimPrefix(line, "seeds:"))
+	seedStrs := strings.Fields(seedList)
+
+	for i := 0; i < len(seedStrs)-1; i += 2 {
+		start := parseInt(seedStrs[i])
+		length := parseInt(seedStrs[i+1])
+
+		for j := start; j < start+length; j++ {
+			allSeeds = append(allSeeds, j)
+		}
+	}
+
+	return allSeeds
+}
+
 func parseInt(s string) int64 {
 	i, _ := strconv.ParseInt(s, 10, 64)
 	return i
 }
 
-func printSeeds(seeds []Seeder) {
-	for _, seed := range seeds {
-		fmt.Printf("seed = %+v\n", seed)
-	}
+func main() {
+	runPart(1)
+	runPart(2)
 }
